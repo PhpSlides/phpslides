08d458a26f9c8e9154bb722fb86c326b9b41c7d4
dconco <onipededavid208@gmail.com>
2024-07-24 19:45:14 +0100

    Updated Command

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use PhpSlides\Foundation\Application;
 
 include dirname(__DIR__) . '/vendor/autoload.php';
 
-Application::configure(rootPath: dirname(__DIR__))
+Application::configure(basePath: dirname(__DIR__))
 	->routing(
 		api: __DIR__ . '/../routes/api.php',
 		web: __DIR__ . '/../routes/web.php'
