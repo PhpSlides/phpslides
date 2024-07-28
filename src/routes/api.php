feb5fd3b410cf28d3683877159dd98fc648cba36
dconco <onipededavid208@gmail.com>
2024-07-28 14:54:51 +0100

    Completed php slides serve command

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,7 +3,4 @@
 use PhpSlides\Http\Api;
 use PhpSlides\Controller\ApiExampleController;
 
-Api::v1()
-	->route('/example/user/{id}', ApiExampleController::class)
-	->middleware(['example'])
-	->name('example.user');
+Api::v1();
